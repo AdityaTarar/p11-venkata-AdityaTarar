c4b49045a4e48e425dc8f2f0b7f7edba3462b6ba
AdityaTarar <adityatarar8@gmail.com>
2023-05-14 11:33:05 +0530

    week 3 session 6 assignments

---
+++ Week-3/Session7/Bootstrap for a vacation website/script.js
@@ -0,0 +1,18 @@
+function showAlert(alertId) {
+  const bootstrapAlert = document.querySelector("#" + alertId);
+  const collapse = new bootstrap.Collapse(bootstrapAlert);
+  collapse.show();
+}
+
+const modalYesBtn = document.querySelector("#contact-modal .btn-primary");
+modalYesBtn.addEventListener("click", function () {
+  const sendBtn = document.querySelector("#send-message-btn");
+  sendBtn.style.display = "none";
+  showAlert("conf-alert");
+});
+document
+  .getElementById("send-message-modal-btn")
+  .addEventListener("click", function () {
+    document.getElementById("send-message-btn").style.display = "none";
+    document.getElementById("conf-alert").classList.remove("collapse");
+  });
